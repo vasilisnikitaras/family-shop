e80bb84f5eac734d4bb27d42423bd144d26c0f20
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-06-28 10:16:07 -0400

    Fix admin cookies code

--- app/admin/admin.tsx
+++ app/admin/admin.tsx
@@ -2,8 +2,9 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import AdminUI from "./AdminUI";
 
-export default function AdminPage() {
-  const cookie = cookies().get("admin_session");
+export default async function AdminPage() {
+  const cookieStore = cookies();
+  const cookie = cookieStore.get("admin_session");
 
   if (!cookie) {
     redirect("/admin/login");
