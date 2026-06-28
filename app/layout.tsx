beff69649defca7b81d0fac906f3711e6150e068
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-06-28 14:22:01 -0400

    Mobile shrink container

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,19 +16,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* PWA manifest */}
         <link rel="manifest" href="/manifest.json" />
-
-        {/* iPhone splash */}
         <link rel="apple-touch-startup-image" href="/splash.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
 
       <body>
-        {children}
+        <div className="container">
+          {children}
+        </div>
 
-        {/* Service worker registration */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
