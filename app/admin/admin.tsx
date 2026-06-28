2720998249f7988832f8148594fa34f639ac7b03
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-06-28 10:59:45 -0400

    fix corrupted admin file

--- app/admin/admin.tsx
+++ app/admin/admin.tsx
@@ -2,9 +2,8 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import AdminUI from "./AdminUI";
 
-export default async function AdminPage() {
-  const cookieStore = cookies();
-  const cookie = cookieStore.get("admin_session");
+export default function AdminPage() {
+  const cookie = cookies().get("admin_session");
 
   if (!cookie) {
     redirect("/admin/login");
